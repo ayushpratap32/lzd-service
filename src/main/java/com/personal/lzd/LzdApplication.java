8a5a69e68a647360680e26f30bfc91b1afefad35
ayushpratap32 <ayushpratap32@gmail.com>
2025-07-12 15:09:16 +0530

    Initial Commit

---
+++ src/main/java/com/personal/lzd/LzdApplication.java
@@ -0,0 +1,13 @@
+package com.personal.lzd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/lzd/LzdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.lzd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
